24a4c423f84d00ca14bde76a181f947d6d9484b1
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2016-11-06 17:23:30 +0100

    Introduces costs module

---
+++ client/app/components/costs/costs.js
@@ -0,0 +1,38 @@
+import angular from 'angular';
+
+import template from './costs.html';
+import './costs.css';
+
+class CostsController {
+  constructor($ngRedux) {
+    'ngInject';
+
+    this.store = $ngRedux;
+  }
+
+  $onInit() {
+    this.unsubscribe = this.store.connect(this.mapStateToThis)(this);
+  }
+
+  $onDestroy() {
+    this.unsubscribe();
+  }
+
+  mapStateToThis(state) {
+    return {
+      currentSubscription: state.subscription
+    }
+  }
+}
+
+const CostsComponent = {
+  template,
+  controller: CostsController,
+  controllerAs: 'costsListCtrl'
+};
+
+const CostsModule = angular.module('costs', [
+  ])
+  .component('costs', CostsComponent);
+
+export { CostsModule, CostsComponent, CostsController };
