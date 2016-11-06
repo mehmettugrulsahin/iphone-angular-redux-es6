4534022c0dda0f9e6dfc7c1905a8fb87a64850fd
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2016-11-06 11:35:21 +0100

    Changes port to 8080

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -33,7 +33,7 @@ gulp.task('serve', () => {
   const compiler = webpack(config);
 
   serve({
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 8080,
     open: false,
     server: { baseDir: root },
     middleware: [
