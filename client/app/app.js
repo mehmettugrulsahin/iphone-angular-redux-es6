a5cafa4bb307fd9dae70af45dedcdb5ca9cedefa
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2016-11-06 14:31:17 +0100

    Adds application main entry point and template

---
+++ client/app/app.js
@@ -0,0 +1,71 @@
+import 'bootstrap-css-only';
+import 'normalize.css';
+
+import angular from 'angular';
+import ComponentsModule from './components/components';
+
+import { combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import ngRedux from 'ng-redux';
+import { subscriptions, subscription } from './components/subscriptions/subscriptions.state';
+
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+import { createDevTools } from 'redux-devtools';
+import LogMonitor from 'redux-devtools-log-monitor';
+import DockMonitor from 'redux-devtools-dock-monitor';
+
+import template from './app.html';
+import './app.css';
+
+const rootReducer = combineReducers({
+  subscriptions,
+  subscription
+});
+
+const DevTools = createDevTools(
+  <DockMonitor toggleVisibilityKey='ctrl-h'
+                changePositionKey='ctrl-q'
+                defaultIsVisible={false}>
+    <LogMonitor theme='tomorrow' />
+  </DockMonitor>
+);
+
+const config = $ngReduxProvider => {
+  'ngInject';
+
+  $ngReduxProvider.createStoreWith(rootReducer, [thunk], [DevTools.instrument()]);
+};
+
+const run = ($ngRedux, $rootScope) => {
+  'ngInject';
+
+  const componentDidUpdate = DockMonitor.prototype.componentDidUpdate;
+  DockMonitor.prototype.componentDidUpdate = function() {
+    $rootScope.$evalAsync();
+    if (componentDidUpdate) {
+      return componentDidUpdate.apply(this, arguments);
+    }
+  };
+
+  ReactDOM.render(
+    <DevTools store={$ngRedux}/>,
+    document.getElementById('devTools')
+  );
+};
+
+const AppComponent = {
+  template
+};
+
+let appModule = angular.module('app', [
+    ComponentsModule.name,
+    ngRedux
+  ])
+  .config(config)
+  .run(run)
+  .component('app', AppComponent)
+;
+
+export default appModule;
